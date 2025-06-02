882b90a357ffe53b5dc98884378bf9ed68ee2b59
DerPommFritz <wiegmannphilipp2002@gmail.com>
2025-06-02 15:08:46 +0200

    Initial sanctified commit of ForgeWorld_M30

---
+++ Source/ForgeWorld_M30/ForgeWorld_M30.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ForgeWorld_M30.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ForgeWorld_M30, "ForgeWorld_M30" );
