57ef6608a0cb315aa0ac3c8e0a99b5cb72bc1325
DerPommFritz <wiegmannphilipp2002@gmail.com>
2025-06-06 15:32:35 +0200

    cleaning the code

--- Source/ForgeWorld_M30/Extractor.h
+++ Source/ForgeWorld_M30/Extractor.h
@@ -16,41 +16,42 @@ class FORGEWORLD_M30_API AExtractor : public AActor
 public:
     AExtractor();
 
-protected:
-    virtual void BeginPlay() override;
-
-public:
     virtual void Tick(float DeltaTime) override;
 
-    /** The resource node this extractor is connected to */
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Extraction")
+    // The resource node this extractor is connected to
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Resource Connection")
     AResourceNode* ConnectedNode;
 
-    /** How often (in seconds) the extractor pulls resources */
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Extraction")
+    // How often (in seconds) the extractor pulls resources 
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Extraction")
     float ExtractionInterval;
 
-    /** Extraction rate in unit per second */
+    // Extraction rate in unit per second
     UPROPERTY(EditAnywhere)
-    float ExtractionRate
+    float ExtractionRate;
 
-    /** Internal Storage */
+    // Internal Storage
     UPROPERTY(VisibleAnywhere)
     TMap<EResourceType, int32> StoredResources;
 
-    /** Internal timer to keep track of extraction time */
+    // Internal timer to keep track of extraction time
     float TimeSinceLastExtraction;
 
-    /** How much to extract each cycle */
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Extraction")
+    // How much to extract each cycle
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Extraction")
     int32 ExtractionAmount;
 
-    /** Performs the actual resource extraction */
+    // Performs the actual resource extraction
     void PerformExtraction();
+
+    // Connect this Extractor to a Resource Node
+    UFUNCTION(BlueprintCallable, Category = "Extractor")
+    void ConnectToNode(AResourceNode* Node);
+
+
+protected:
+    virtual void BeginPlay() override;
 
 private:
     float ExtractionAccumulator;
 };
-
-UFUNCTION(BlueprintCallable, Category = "Extractor")
-void ConnectToNode(AResourceNode* Node);
